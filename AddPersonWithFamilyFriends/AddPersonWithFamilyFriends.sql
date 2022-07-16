2ef2a40ace3f305e0b9fddda3df99565d988ed57
Luv sahu <lksahu404@gmail.com>
2022-07-16 17:45:22 +0530

    [Luv SAHU] Those Query for Add Peerson With Family Friends

---
+++ AddPersonWithFamilyFriends/AddPersonWithFamilyFriends.sql
@@ -0,0 +1,6 @@
+----------uc11 - ability to add the person to both the family and friends----
+INSERT INTO AddressBook
+VALUES('Siyram','Chaudhary','6th cross','Gurgaon','Haryana',765434,457545809,'vwx@gmail.com','Friend'),
+('Priyanka','Chaudhary','6th cross','Patna','Bihar',765434,457545809,'vwx@gmail.com','Family')
+
+Select * from AddressBook;
