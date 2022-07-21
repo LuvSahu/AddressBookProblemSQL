cb67377888957016bc441cd4bc57c3a705576506
Luv <104768173+LuvSahu@users.noreply.github.com>
2022-07-21 17:39:21 +0530

    Delete RetrivePersonOnCityOrState directory

--- RetrivePersonOnCityOrState/RetrivePersonOnCityOrState.sql
+++
@@ -1,9 +0,0 @@
-select * from AddressBook;
------------inserted some more data to execute the uc--------------
-INSERT INTO AddressBook
-Values('Rahul','Raj','9th line Road','Kota','Rajasthan',560087,1234567897,'lkj@gmail.com'),
-('Rishi','Dutt','Main Road','Ajmer','Rajasthan',560097,1234567807,'omn@gmail.com'),
-('Shivam','Raj','145 Block Shan Road','Nuh','Haryana',561237,1653267897,'ytr@gmail.com');
-
--------------UC6-retrive the person bolonging to coty or state--------------
-SELECT FirstName From AddressBook where  City='Ajmer' or State='Rajasthan';
