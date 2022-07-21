6d8468779c1733008db085d52816c814777c3e34
Luv <104768173+LuvSahu@users.noreply.github.com>
2022-07-21 17:40:02 +0530

    Delete InsertDataIntoTable directory

--- InsertDataIntoTable/InsertDataIntoTable.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO AddressBook
-VALUES('Luv','Sahu','Shan Road','Jaipur','Rajasthan',560065,5243454345,'abc@gmail.com'),
-('Deepak','Saini','NEB Road','Dehli','Dehli',560066,1323567775,'duf@gmail.com'),
-('Atul','Gupta','Katla Road','Ajmer','Rajasthan',450065,5478908765,'lip@gmail.com'),
-('Rohit','katariya','Bhagat Singh Road','Jaipur','Rajasthan',660065,5243458745,'jkl@gmail.com');
